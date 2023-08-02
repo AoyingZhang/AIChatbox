1df3f08b303ecc7d0f536196804fb6d520d3313e
Lexie Zhang <107318892+AoyingZhang@users.noreply.github.com>
2023-08-01 23:30:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ import os
 load_dotenv()  # take environment variables from .env.
 
 
-from google.cloud import firestore
+# from google.cloud import firestore
 
-# Authenticate to Firestore with the JSON account key.
-import json
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+# # Authenticate to Firestore with the JSON account key.
+# import json
+# key_dict = json.loads(st.secrets["textkey"])
+# creds = service_account.Credentials.from_service_account_info(key_dict)
+# db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
+# # Create a reference to the Google post.
+# doc_ref = db.collection("posts").document("Google")
 
-# Then get the data at that reference.
-doc = doc_ref.get()
+# # Then get the data at that reference.
+# doc = doc_ref.get()
 
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+# # Let's see what we got!
+# st.write("The id is: ", doc.id)
+# st.write("The contents are: ", doc.to_dict())
 
 st.title('Welcome to the AI Chatbox web! ')
 st.subheader('Please upload the _PDF_ file you want to get help about')
