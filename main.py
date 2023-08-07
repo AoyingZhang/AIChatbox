145846c43c5de082b3dbc12608ca1eb3a4f1a231
Lexie Zhang <zay20040811@gmail.com>
2023-08-07 13:11:10 -0400

    added getfile and putfile function to database

--- main.py
+++ main.py
@@ -6,13 +6,45 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 import openai
 from google.oauth2 import service_account
-
+from google.cloud import firestore
 from dotenv import load_dotenv
 import os
 load_dotenv()  # take environment variables from .env.
 
 
-from google.cloud import firestore
+#generate prompt template ->set up for AI
+template_string = """Please handle the text \
+that is delimited by triple backticks based on\
+the following requirements that is delimited by triple pipes\
+text: ```{text}```\
+requirements:|||{requirements}|||\
+"""
+prompt_template = ChatPromptTemplate.from_template(template_string)
+
+#add file to database function
+def addFile(db, docName, name, text):
+    doc_ref = db.collection('users').document(docName)
+    doc_ref.set({
+        'name': name,
+        'content': text
+    })
+
+def getFile(db, docName):
+    doc_ref = db.collection("users").document(docName)
+    doc = doc_ref.get()
+    if doc.exists:
+        return doc.to_dict()['content']
+
+    else: 
+        print("No such document!")
+
+#get AI response function
+def getAIResponse(prompt_template, content, user_input):
+    if(user_input!=''):
+        messages = prompt_template.format_messages(text=content, requirements=user_input)
+        response = chat(messages)
+        return response.content
+    return ''
 
 # Authenticate to Firestore with the JSON account key.
 import json
@@ -20,57 +52,54 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="ai-chatbox-database")
 
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
-
-# Then get the data at that reference.
-doc = doc_ref.get()
 
 #Database info
 #st.write("The id is: ", doc.id)
 #st.write("The contents are: ", doc.to_dict())
 
+#UI header
 st.title('Welcome to the AI Chatbox web! ')
 st.subheader('Please upload the _PDF_ file you want to get help about')
 st.subheader('Then a chatbox will appear and what you need help about :sunglasses:')
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
-print(openai_api_key) 
 openai.api_key = openai_api_key
 
 # set up using Langchain
 chat = ChatOpenAI(openai_api_key=openai_api_key, temperature=0.0)
-
-
 uploaded_file = st.file_uploader("Choose a PDF file")
+content=''
 
+
+
+
+
+
+
+
+#upload file to the database
 if uploaded_file is not None:
     #read file
     reader = PdfReader(uploaded_file)
-    
-    # printing number of pages in pdf file
-    print(len(reader.pages))
-    
+
+    #convert to text
     content = ''
     for i in range(len(reader.pages)):
         page = reader.pages[i]
         text = page.extract_text()
         content = content+text
-    #content
+    
+    #get person name and doc name
+    name= getAIResponse(prompt_template, content, 'What is the name of the person? Please just answer 2 words.')
+    docName = name+' Resume'
+    addFile(db, docName, name, text)
 
-    user_input = st.text_input('How can I help you?', 'Please summarize the text content')
-
-    template_string = """Please handle the text \
-    that is delimited by triple backticks based on\
-    the following requirements that is delimited by triple pipes\
-    text: ```{text}```\
-    requirements:|||{requirements}|||\
-    """
-    prompt_template = ChatPromptTemplate.from_template(template_string)
-    messages = prompt_template.format_messages(text=content, requirements=user_input)
-    response = chat(messages)
-    "\nHere's the response: \n"
-    response.content
+#get answer
+user_input = st.text_input('How can I help you?', '')
+getResumeName= getAIResponse(prompt_template, user_input, 'What is the name of the person we are getting resume about? Please just answer 2 words.')+" Resume"
+getResumeContent = getFile(db, getResumeName)
+print(getResumeName)
+st.write(getAIResponse(prompt_template, getResumeContent, user_input))
 
 
 
